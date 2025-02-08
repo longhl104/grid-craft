40ee1f5135bc0c6af8116e189dc81af973d9ad94
Duy Long Nguyen <longlunglay1998@gmail.com>
2025-02-09 10:26:37 +1100

    Initialize CDK project structure with basic stack and configuration files

---
+++ GridCraftCdk/src/GridCraftCdk/GridCraftCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace GridCraftCdk
+{
+    public class GridCraftCdkStack : Stack
+    {
+        internal GridCraftCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
